6be1a34fee7fadb2252bc51edc68d09caa35f2e2
M-Shourav <muhammadshourav625@gmail.com>
2024-10-04 10:36:02 +0600

    resume added

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -10,7 +10,7 @@ const Resume = () => {
       className="py-20 font-titleFont border border-transparent border-b-black"
     >
       <div className="flex items-center justify-center text-center">
-        <Title title="1+ YEARS OF EXPERIENCE" des="My Resume" />
+        <Title title="Fresher" des="My Resume" />
       </div>
       <div>
         <ul className="w-full grid grid-cols-1 sm:grid-cols-4">
@@ -20,9 +20,44 @@ const Resume = () => {
           <li className="resumeLi">Achievements</li>
         </ul>
       </div>
-      <div className="py-10 font-titleFont">
-        <p className="text-sm text-designColor tracking-[4px]">2018-2024</p>
-        <h2 className="text-4xl font-semibold">Education Quality</h2>
+      <div className="w-full flex flex-col sm:flex-row gap-10">
+        <div>
+          <div className="py-10 font-titleFont">
+            <p className="text-sm text-designColor tracking-[4px]">2018-2024</p>
+            <h2 className="text-4xl font-semibold">Education Quality</h2>
+          </div>
+          <div className="mt-10 w-full h-[700px] border-l-[6px] border-l-black flex flex-col gap-10">
+            <ResumeCard
+              title="Diploma In Computer Engineer"
+              subtitle="Dinajpur Polytechnic Institute (2020-2024)"
+              result="3.81/4"
+              des="The training provided by Bangladesh Technical Education Board (BTEB) in order to prepare people to work in various sectors of the economy or areas of culture."
+            />
+            <ResumeCard
+              title="Secondary School Education"
+              subtitle="Chilahati Merchant's High School (2018-2020)"
+              result="4.72/5"
+              des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
+            />
+          </div>
+        </div>
+        <div>
+          <div className="py-10 font-titleFont">
+            <p className="text-sm text-designColor tracking-[4px]">
+              06/2024-09/2024
+            </p>
+            <h2 className="text-4xl font-semibold">Industrial Training</h2>
+          </div>
+          <div className="mt-10 w-full h-[350px] border-l-[6px] border-l-black flex flex-col gap-10">
+            <ResumeCard
+              title="MERN Stack Development"
+              subtitle="PeopleNTech It Training Center"
+              result="Dhaka"
+              des="Passionate and skilled MERN stack developer with a
+              strong foundation in MongoDB, Express.js, React, and Node.js."
+            />
+          </div>
+        </div>
       </div>
     </Container>
   );
--- src/components/resume/ResumeCard.js
+++ src/components/resume/ResumeCard.js
@@ -1,7 +1,33 @@
 import React from "react";
 
-const ResumeCard = () => {
-  return <div>ResumeCard</div>;
+const ResumeCard = ({ title, subtitle, result, des }) => {
+  return (
+    <div className="w-full h-full group flex">
+      <div className="w-10 h-[6px] bg-black mt-16 relative">
+        <span className="w-5 h-5 bg-black rounded-full absolute -top-2 -left-3 flex items-center justify-center">
+          <span className="w-3 h-3 rounded-full bg-bodyColor inline-flex group-hover:bg-designColor duration-300" />
+        </span>
+      </div>
+      <div
+        className="w-full bg-black bg-opacity-20 group-hover:bg-opacity-30  rounded-lg duration-300 px-10 flex
+       flex-col justify-center gap-10 shadow-shadowOne"
+      >
+        <div>
+          <h1 className="text-sm sm:text-2xl  sm:font-semibold">{title}</h1>
+          <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-3 mt-2">
+            <p className="text-xs sm:text-base ">{subtitle}</p>
+            <p
+              className="w-20 h-10 text-designColor bg-black flex items-center 
+            justify-center text-sm font-semibold rounded-lg shadow-shadowOne"
+            >
+              {result}
+            </p>
+          </div>
+          <p className="text-sm font-semibold mt-10">{des}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ResumeCard;
